3e5892321d7ad58905f2426a5c4b2653e84ebd62
ertanay <ertanay8@gmail.com>
2022-03-30 14:54:42 +0300

    Web Service
    C# ile SQL den web siteme veri çektim.

---
+++ wttprojesi/GetDatamx.asmx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using wttprojesi.database;
+
+namespace wttprojesi
+{
+    /// <summary>
+    /// Summary description for GetDatamx
+    /// </summary>
+    [WebService(Description = "Ödev")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class GetDatamx : System.Web.Services.WebService
+    {
+        wttProjesi db = new wttProjesi();
+        [WebMethod]
+        public List<Personel> MeslekVeKanGrubu(string meslek, string kanGrubu)
+        {
+            return db.Personel.Where(x => x.Meslek.Contains(meslek) && x.kan_grubu == kanGrubu).ToList();
+        }
+        [WebMethod]
+        public List<Personel> BölümVeDaire(string bolum, string daire)
+        {
+            return db.Personel.Where(x => x.Bolum.Contains(bolum) && x.Daire.Contains(daire)).ToList();
+        }
+    }
+
+}
---
+++ wttprojesi/obj/Release/Package/PackageTmp/GetDatamx.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="GetDatamx.asmx.cs" Class="wttprojesi.GetDatamx" %>
